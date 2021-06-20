7728ae4b1c10659b10f0d11181c45b0ca28740d3
Ivan Filipchik <wot.vania@mail.ru>
2021-06-21 01:51:10 +0300

    small refactoring

--- src/components/Skills/Skill/Skill.js
+++ src/components/Skills/Skill/Skill.js
@@ -9,9 +9,9 @@ function Skill(props) {
             <h3>
                 {props.title}
             </h3>
-            <span className={s.description}>
+            <p className={s.description}>
                 {props.description}
-            </span>
+            </p>
         </div>
     );
 }
