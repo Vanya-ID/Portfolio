723a6903eeab515962525347c9b38b41a17a383d
Ivan Filipchik <wot.vania@mail.ru>
2021-07-10 01:07:44 +0300

    Fix skills block

--- src/components/Skills/Skill/Skill.js
+++ src/components/Skills/Skill/Skill.js
@@ -4,7 +4,7 @@ function Skill(props) {
     return (
         <div className={s.skill}>
             <div className={s.icon}>
-
+                <img src={props.img} alt=""/>
             </div>
             <h3>
                 {props.title}
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,22 +5,31 @@ function Skills() {
     return (
         <div className={s.skillsBlock}>
             <div className={s.container}>
-                <h2 className={s.title}>Мои Скиллы</h2>
+                <h2 className={s.title}>My Skills</h2>
                 <div className={s.skills}>
-                    <Skill title={'JS'}
-                           description={'Hello JS'}
+                    <Skill title={'React'}
+                           img={'https://img.icons8.com/wired/100/26e07f/react.png'}
+                           description={'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.'}
                     />
                     <Skill title={'CSS'}
-                           description={'Hello CSS'}
+                           img={'https://img.icons8.com/ios-filled/50/26e07f/css3.png'}
+                           description={'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.'}
                     />
-                    <Skill title={'REACT'}
-                           description={'Hello REACT'}
+                    <Skill title={'JavaScript'}
+                           img={'https://img.icons8.com/ios-filled/50/26e07f/javascript.png'}
+                           description={'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.'}
+                    />
+                    <Skill title={'TypeScript'}
+                           img={'https://img.icons8.com/ios-filled/50/26e07f/typescript.png'}
+                           description={'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.'}
+                    />
+                    <Skill title={'Redux'}
+                           img={'https://img.icons8.com/ios-filled/50/26e07f/redux.png'}
+                           description={'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.'}
                     />
                     <Skill title={'Jest'}
-                           description={'Hello REACT'}
-                    />
-                    <Skill title={'REACT'}
-                           description={'Hello REACT'}
+                           img={'https://img.icons8.com/ios-filled/50/26e07f/test-tube.png'}
+                           description={'Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.'}
                     />
                 </div>
             </div>
