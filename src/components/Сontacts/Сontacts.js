66305c8d5443167cd0bc294695b236f61340a0c2
Ivan Filipchik <wot.vania@mail.ru>
2021-07-10 11:16:37 +0300

    Get in Touch Block

--- src/components/Сontacts/Сontacts.js
+++ src/components/Сontacts/Сontacts.js
@@ -1,21 +1,27 @@
 import c from './Сontacts.module.scss'
-import s from "../Skills/Skills.module.scss";
 
 function Contacts() {
     return (
         <div className={c.contactBlock}>
             <div className={c.container}>
-
                 <form className={c.form} action="">
                     <h2 className={c.title}>
-                       Get in Touch
+                        Get in Touch
                     </h2>
                     <div className={c.contacts}>
-                        <input type="text" placeholder={'Телефон'}/>
-                        <input type="text" placeholder={'Почта'}/>
-                        <textarea name="" id="" cols="30" rows="10" placeholder={'text'}/>
+                        <div className={c.info}>
+                            <input type="text" placeholder={'Name'}/>
+                            <input type="email" placeholder={'Email'}/>
+                        </div>
+                        <div>
+                            <textarea name="" id="" cols="30" rows="10"
+                                      placeholder={'Tell me more about your needs...'}/>
+                        </div>
+                        <button className={c.btn}>
+                            Send Message
+                        </button>
+
                     </div>
-                    <button>Отправить</button>
                 </form>
             </div>
         </div>
