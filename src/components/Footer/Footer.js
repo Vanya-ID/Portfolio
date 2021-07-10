9129591c84f9952263f16fcaf53a28b8ac50b7f0
Ivan Filipchik <wot.vania@mail.ru>
2021-07-10 11:44:14 +0300

    fix contact block and add href to icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,20 @@ function Footer() {
     return (
         <div className={f.footerBlock}>
             <div className={f.container}>
-                <h2>Ivan</h2>
+                <h2 className={f.title}>Ivan</h2>
                 <div className={f.logos}>
-                    <img src="https://goo.su/66UX" alt=""/>
-                    <img src="https://goo.su/66uy" alt=""/>
-                    <img src="https://goo.su/66uY" alt=""/>
+                    <a href='https://vk.com/king_of_dumplings'>
+                        <img src="https://img.icons8.com/color/48/000000/vk-circled.png" alt=""/>
+                    </a>
+                    <a href='https://www.instagram.com/66_noname/?hl=ru'>
+                        <img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png" alt=""/>
+                    </a>
+                    <a href='https://www.linkedin.com/in/vanya-id-a13784205/'>
+                        <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt=""/>
+                    </a>
+                    <a href='https://github.com/Vanya-afk?tab=repositories'>
+                        <img src="https://img.icons8.com/ios-glyphs/30/4a90e2/github.png" alt=""/>
+                    </a>
                 </div>
                 <span>©2021 Все права защищены </span>
             </div>
