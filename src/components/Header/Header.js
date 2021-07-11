f1927cc7cdb2a3267da7055919a021a09b3e76de
Ivan Filipchik <wot.vania@mail.ru>
2021-07-11 08:41:10 +0300

    smooth animation to menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,33 @@
 import h from './Header.module.scss'
 import Nav from "../Nav/Nav";
+import {useEffect, useState} from "react";
 
 function Header() {
+    const [offset, setOffset] = useState(0);
+
+    let style = {};
+    useEffect(() => {
+        window.onscroll = () => {
+            setOffset(window.pageYOffset)
+        }
+    }, []);
+    if (offset >= 90 && offset <= 120) {
+        style = {
+            position: 'fixed',
+            backgroundColor: '#111',
+            transform: 'translateY(-100%)'
+        }
+    }
+    if(offset>=115) {
+        style={
+            position: 'fixed',
+            backgroundColor: '#111',
+            transform: 'translateY(0%)',
+            transition: '0.5s',
+        }
+    }
     return (
-        <div className={h.headerBlock}>
+        <div style={style} className={h.headerBlock}>
             <Nav/>
         </div>
     );
